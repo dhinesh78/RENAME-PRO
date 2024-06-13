6195e70d2c5f098e23f01f4619d7738e3bb866fa
dhinesh78 <144138957+dhinesh78@users.noreply.github.com>
2024-06-12 22:23:44 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6380339169:AAH5vkzcx8Y2ZaRhJURV4eEcah332uAjO0c")
+API_ID = int(os.environ.get("API_ID", "20652787"))
+API_HASH = os.environ.get("API_HASH", "5dea928561e4d2eb77a371edf8b2eb2a")
+OWNER = int(os.environ.get("OWNER", "1242556540"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Files_renamer_Dp_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "dp_botz")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Aloneboy:Aloneboytg@cluster0.nmeelsr.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
 
 STRING = os.environ.get("STRING", "")
